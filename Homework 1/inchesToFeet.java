65ea40a2ac829d780ec72eef419ec7ec7182dcdd
shailis17 <83250817+shailis17@users.noreply.github.com>
2021-07-15 13:02:15 -0400

    Add files via upload

---
+++ Homework 1/inchesToFeet.java
@@ -0,0 +1,16 @@
+package homework1;
+
+public class inchesToFeet 
+{
+
+	public static void main(String[] args) 
+	{
+		int inches = 28;
+		
+		double feet = inches/12.0;
+		
+		System.out.print(inches + "in = " + feet + "ft.");
+
+	}
+
+}
